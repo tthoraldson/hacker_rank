69923351d1d80cecc0002025ca527867f868a22c
Theresa Thoraldson <theresa.thoraldson@gmail.com>
2016-04-29 15:14:03 -0500

    4/29/2016
    started 30 days of code

---
+++ 30_days_of_code/day_0.py
@@ -0,0 +1,10 @@
+# Completed on 4/28/2016 by Theresa Thoraldson
+# source: https://www.hackerrank.com/challenges/30-hello-world
+
+inputString = input() # get a line of input from stdin and save it to our variable
+
+# Your first line of output goes here
+print('Hello, World.')
+print(inputString)
+
+# Write the second line of output
---
+++ 30_days_of_code/day_1.py
@@ -0,0 +1,22 @@
+# Completed on 4/29/2016 by Theresa Thoraldson
+# source: https://www.hackerrank.com/challenges/30-data-types
+
+# Declare second integer, double, and String variables.
+x = 0
+y = 5.0
+z = 'randomrandom'
+
+# Read and save an integer, double, and String to your variables.
+x = int(raw_input())
+y = raw_input()
+z = raw_input()
+
+# Print the sum of both integer variables on a new line.
+print i + x
+
+# Print the sum of the double variables on a new line.
+print float(y) + d
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s + z
