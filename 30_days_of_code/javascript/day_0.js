204e5c11caef4e0bdf2dca854ca1c7d01594a05c
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2016-06-23 16:06:48 -0500

    folder change

---
+++ 30_days_of_code/javascript/day_0.js
@@ -0,0 +1,11 @@
+# Completed on 6/23/2016 by Theresa Thoraldson
+# source: https://www.hackerrank.com/challenges/30-hello-world
+
+function processData(inputString) {
+    // Your first line of output goes here
+    process.stdout.write("Hello, World.");
+    
+    // Write the second line of output
+    process.stdout.write("\n" + inputString)
+} 
+
