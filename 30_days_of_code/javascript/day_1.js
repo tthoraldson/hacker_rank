d4471b1d9edf91a7e6aefab5eff69b4ee4c02af6
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2016-07-19 19:54:53 -0500

    HR .js day 1!

---
+++ 30_days_of_code/javascript/day_1.js
@@ -0,0 +1,8 @@
+# Completed on 7/19/2016 by Theresa Thoraldson
+# source: https://www.hackerrank.com/challenges/30-data-types
+
+var x = Number(readLine(input_stdin));
+var y = Number(readLine(input_stdin));
+var z = readLine(input_stdin);
+
+process.stdout.write(i + x + "\n" + (y + d).toFixed(1) + "\n" + s + z);
