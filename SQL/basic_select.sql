174761a4184d35852a9e97ee93a224f83f55d95a
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2016-11-28 20:24:09 -0600

    basic select clean up

---
+++ SQL/basic_select.sql
@@ -0,0 +1,50 @@
+-- revising the select query I
+SELECT *
+FROM city
+WHERE (POPULATION > 100000) AND (COUNTRYCODE = "USA");
+
+-- revising the select query II
+SELECT NAME
+FROM CITY
+WHERE (POPULATION > 120000) AND (COUNTRYCODE = "USA");
+
+-- select all
+SELECT *
+FROM CITY;
+
+-- select by ID
+SELECT *
+FROM CITY
+WHERE (ID = 1661);
+
+-- japanese cities attributes
+SELECT *
+FROM CITY
+WHERE (COUNTRYCODE = "JPN");
+
+-- japanese cities name
+SELECT NAME
+FROM CITY
+WHERE (COUNTRYCODE = "JPN");
+
+-- weather observation station 1
+SELECT CITY, STATE
+FROM STATION;
+
+-- weather obvservation station 2
+SELECT ROUND(SUM(LAT_N), 2), ROUND(SUM(LONG_W), 2)
+FROM STATION;
+
+-- weather observation station 3
+SELECT DISTINCT CITY
+FROM STATION
+WHERE MOD (ID, 2) = 0;
+
+-- weather observation station 4
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
+FROM STATION;
+
+-- weather observation station 13
+SELECT ROUND(SUM(LAT_N), 4)
+FROM STATION
+WHERE (LAT_N > 38.7880) AND (LAT_N < 137.3245);
